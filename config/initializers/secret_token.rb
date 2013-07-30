92a319555a9ef26c9227cf0705901bd2ad782dcc
Ankoor Patel <ankoor.p@gmail.com>
2013-07-30 16:33:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TflCameras::Application.config.secret_token = '1cd60ae241ebf889e423b54a793ae54c4d71b688c0e6eb9e770e391f9a5dab0d29bd303628cedd85eaea979fc20ae535b13571392bd996df0268070318fc2f05'
